417038b57113e2be9920351c94d5c55218037935
marcwue <Eagles@192.168.1.50>
2012-06-05 18:18:59 +0200

    class symbolTable vergessen

---
+++ src/descriptoren/SymbolTable.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package descriptoren;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Marc Wüseke
+ * 
+ */
+public class SymbolTable {
+
+	private Map<String, AbstractDescr> descriptorMap = new HashMap<String, AbstractDescr>();
+	private Map<String, Integer> addressMap = new HashMap<String, Integer>();
+	private int currentAddress = 0;
+	private SymbolTable parentTable;
+
+	/**
+	 * @param parentTable
+	 */
+	public SymbolTable(SymbolTable parentTable) {
+		super();
+		this.parentTable = parentTable;
+	}
+
+	public void declare(String ident, AbstractDescr descr) {
+		if (!(addressMap.containsKey(ident))) {
+			descriptorMap.put(ident, descr);
+			addressMap.put(ident, currentAddress);
+			if (descr == null) {
+				System.out.println("---- compile Error-----\n Variable = "
+						+ ident);
+			}
+			currentAddress += descr.getSize();
+		} else {
+			System.out
+					.println("Fehler, zweimal die gleiche Variable deklariert");
+		}
+	}
+
+	public AbstractDescr descriptorFor(String ident) {
+		// built-in types
+		if (ident.equalsIgnoreCase("integer")) {
+			return new SimpleTypeDescr(1, "INTEGER");
+		}
+
+		AbstractDescr d = descriptorMap.get(ident);
+		if (d == null && parentTable != null) {
+			return parentTable.descriptorFor(ident);
+		}
+		return d;
+	}
+
+	public int addressOf(String ident) {
+
+		if (addressMap.containsKey(ident)) {
+			return addressMap.get(ident);
+		}
+		if (parentTable != null) {
+			return parentTable.addressOf(ident);
+		}
+		return -1;
+
+	}
+
+	public int size() {
+		return currentAddress;
+	}
+
+}
