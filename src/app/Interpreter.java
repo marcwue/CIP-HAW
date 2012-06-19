10d3ec90c18beebd97dc994ed4480b116ffac96b
marcwue <Eagles@192.168.1.50>
2012-06-19 17:06:18 +0200

    dooooof

--- src/app/Interpreter.java
+++ src/app/Interpreter.java
@@ -1,9 +1,14 @@
 package app;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+
+import cip.standaloneCodeGen.StandaloneCG;
 
 import descriptoren.SymbolTable;
 
@@ -11,19 +16,20 @@ import nodes.AbstractNode;
 
 public class Interpreter {
 
-	private final static String nodeFile = "nodeFile.txt";
+	private final static String nodeFile = "nodeFile";
 	static MyFlexScanner scanner = null;
 	static SymbolTable symbolTable = new SymbolTable();
 
 	public static void main(String[] argv) {
 		System.out.println("MyStandalone Version 0.1");
-
+		
 		if (argv.length == 0) {
 			System.out.println("Usage : java MyStandalone <inputfile>");
 		} else {
 
 			for (int i = 0; i < argv.length; i++) {
 
+				String datei = "src/app/TestDaten.txt";
 				try {
 					System.out.println("### Scan:");
 					scanner = new MyFlexScanner(new java.io.FileReader(argv[i]));
@@ -61,6 +67,15 @@ public class Interpreter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("### Standalone fu");
+		String[] s = {"nodefile","1"};
+		try {
+			StandaloneCG.main(s);
+		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
