08e39aa049b64de78c5a372856a423eba52151f9
Benjamin Kahlau <ngk-ben@gmx.de>
2012-06-21 16:22:21 +0200

    linkage in den Konstruktor verschoben

--- src/nodes/ProcedureBodyNode.java
+++ src/nodes/ProcedureBodyNode.java
@@ -5,107 +5,107 @@ import descriptoren.ProcDescr;
 import descriptoren.SymbolTable;
 
 public class ProcedureBodyNode extends AbstractNode {
-	private final DeclarationsNode declarations;
-	private final AbstractNode statementseq;
-	private final int linkage = 3;
-	private IdentNode decIdent;
+    private final DeclarationsNode declarations;
+    private final AbstractNode statementseq;
+    private final int linkage;
+    private IdentNode decIdent;
 
-	public ProcedureBodyNode(IdentNode decIdent, DeclarationsNode declarations, AbstractNode statementseq) {
-		this.decIdent = decIdent;
-		this.declarations = declarations;
-		this.statementseq = statementseq;
-	}
+    public ProcedureBodyNode(IdentNode decIdent, DeclarationsNode declarations, AbstractNode statementseq) {
+        this.decIdent = decIdent;
+        this.declarations = declarations;
+        this.statementseq = statementseq;
+        this.linkage = 3;
+    }
 
-	public AbstractDescr compile(SymbolTable symbolTable, SymbolTable symbolTableL, int pushBackSize) {
+    public AbstractDescr compile(SymbolTable symbolTable, SymbolTable symbolTableL, int pushBackSize) {
 
-		int label = getNextLabelNumber();
+        int label = getNextLabelNumber();
 
-		if (declarations != null) {
-			declarations.compile(symbolTableL);
-		}
-		int allocatedMemory = linkage + symbolTableL.getSize();
-		write("LABEL, " + label);
+        if (declarations != null) {
+            declarations.compile(symbolTableL);
+        }
+        int allocatedMemory = linkage + symbolTableL.getSize();
+        write("LABEL, " + label);
 
-		//start
-		write("INIT, " + allocatedMemory);
-		write("PUSHREG, RK");
-		write("PUSHREG, FP");
-		//rette sich wer kann
-		write("PUSHI, " + symbolTableL.getLevel());
-		write("PUSHREG, SL");
-		// fp auf stack pointer addresse setzen
-		write("GETSP");
-		write("SETFP");
-		// sl auf frame pointer setzen
-		write("GETFP");
-		write("PUSHI, " + symbolTableL.getLevel());
-		write("SETSL");
-		// SP := SP + lokale variablen
-		write("GETSP");
-		write("PUSHI, " + symbolTableL.getSize());
-		write("ADD");
-		write("SETSP");
-		// end
+        //start
+        write("INIT, " + allocatedMemory);
+        write("PUSHREG, RK");
+        write("PUSHREG, FP");
+        //rette sich wer kann
+        write("PUSHI, " + symbolTableL.getLevel());
+        write("PUSHREG, SL");
+        // fp auf stack pointer addresse setzen
+        write("GETSP");
+        write("SETFP");
+        // sl auf frame pointer setzen
+        write("GETFP");
+        write("PUSHI, " + symbolTableL.getLevel());
+        write("SETSL");
+        // SP := SP + lokale variablen
+        write("GETSP");
+        write("PUSHI, " + symbolTableL.getSize());
+        write("ADD");
+        write("SETSP");
+        // end
 
-		if (statementseq != null) {
-			System.out.println("ssssssssssss"+statementseq);
-			statementseq.compile(symbolTableL);
-		} else {
-			System.out.println("no StatementSequenzNode");
-		}
+        if (statementseq != null) {
+            statementseq.compile(symbolTableL);
+        } else {
+            System.out.println("no StatementSequenzNode");
+        }
 
-		// alles wieder zurück bauen
-		write("GETFP");
-		write("SETSP");
-		// sl zurücksetzen
-		write("PUSHI, " + symbolTableL.getLevel());
-		write("POPREG, SL");
-		write("POPREG, FP");
-		write("POPREG, RK");
-		// stackpointer zurückverschieben
-		write("GETSP");
-		write("PUSHI, " + pushBackSize);
-		write("SUB");
-		write("SETSP");
+        // alles wieder zurück bauen
+        write("GETFP");
+        write("SETSP");
+        // sl zurücksetzen
+        write("PUSHI, " + symbolTableL.getLevel());
+        write("POPREG, SL");
+        write("POPREG, FP");
+        write("POPREG, RK");
+        // stackpointer zurückverschieben
+        write("GETSP");
+        write("PUSHI, " + pushBackSize);
+        write("SUB");
+        write("SETSP");
 
-		int reduceVal = allocatedMemory + pushBackSize;
-		write("REDUCE, " + reduceVal);
-		write("RET");
+        int reduceVal = allocatedMemory + pushBackSize;
+        write("REDUCE, " + reduceVal);
+        write("RET");
 
-		ProcDescr descr = new ProcDescr(label, symbolTableL);
-		symbolTable.declare(decIdent.getIdentName(), descr);
-		return descr;
-	}
+        ProcDescr descr = new ProcDescr(label, symbolTableL);
+        symbolTable.declare(decIdent.getIdentName(), descr);
+        return descr;
+    }
 
-	@Override
-	public String toString() {
-		return "ProcedureBodyNode{" + "declarations=" + declarations
-				+ ", statseq=" + statementseq + '}';
-	}
+    @Override
+    public String toString() {
+        return "ProcedureBodyNode{" + "declarations=" + declarations
+                + ", statseq=" + statementseq + '}';
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
-		ProcedureBodyNode that = (ProcedureBodyNode) o;
+        ProcedureBodyNode that = (ProcedureBodyNode) o;
 
-		if (declarations != null ? !declarations.equals(that.declarations)
-				: that.declarations != null)
-			return false;
-		if (statementseq != null ? !statementseq.equals(that.statementseq)
-				: that.statementseq != null)
-			return false;
+        if (declarations != null ? !declarations.equals(that.declarations)
+                : that.declarations != null)
+            return false;
+        if (statementseq != null ? !statementseq.equals(that.statementseq)
+                : that.statementseq != null)
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		int result = declarations != null ? declarations.hashCode() : 0;
-		result = 31 * result + (statementseq != null ? statementseq.hashCode() : 0);
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        int result = declarations != null ? declarations.hashCode() : 0;
+        result = 31 * result + (statementseq != null ? statementseq.hashCode() : 0);
+        return result;
+    }
 }
