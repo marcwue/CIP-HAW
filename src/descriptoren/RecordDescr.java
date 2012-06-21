02d8154cf8cd23c79f36399e52f8d984cb4e4964
Benjamin Kahlau <ngk-ben@gmx.de>
2012-06-21 16:36:44 +0200

    Fehler für falsche RecordGröße gefunden

--- src/descriptoren/RecordDescr.java
+++ src/descriptoren/RecordDescr.java
@@ -1,53 +1,51 @@
 package descriptoren;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author Marc Wüseke
  */
 public class RecordDescr extends AbstractDescr {
-	private static final long serialVersionUID = 1L;
-	SymbolTable recSymbolTable;
+    private static final long serialVersionUID = 1L;
+    SymbolTable recSymbolTable;
 
-	public RecordDescr(SymbolTable recSymbolTable) {
-		this.recSymbolTable = recSymbolTable;
-	}
+    public RecordDescr(SymbolTable recSymbolTable) {
+        this.recSymbolTable = recSymbolTable;
+    }
 
-	public SymbolTable fields() {
-		return this.recSymbolTable;
-	}
+    public SymbolTable fields() {
+        return this.recSymbolTable;
+    }
 
-	public int size() {
-		return recSymbolTable.getSize();
-	}
+    @Override
+    public int getSize() {
+        return recSymbolTable.getSize();
+    }
 
-	public String toString() {
-		String b = "";
-		b += "RecordDescriptor[ ";
-		b += recSymbolTable.toString();
-		b += " ] ";
-		return b;
-	}
+    public String toString() {
+        String b = "";
+        b += "RecordDescriptor[ ";
+        b += recSymbolTable.toString();
+        b += " ] ";
+        return b;
+    }
 
-	public AbstractDescr descriptorFor(String s) {
-		return recSymbolTable.descriptorFor(s);
-	}
+    public AbstractDescr descriptorFor(String s) {
+        return recSymbolTable.descriptorFor(s);
+    }
 
-	public int addressOf(String s) {
-		return recSymbolTable.addressOf(s);
-	}
+    public int addressOf(String s) {
+        return recSymbolTable.addressOf(s);
+    }
 
-	public boolean equals(Object o) {
-		if (!(o instanceof RecordDescr))
-			return false;
-		RecordDescr d = (RecordDescr) o;
-		return this.recSymbolTable.equals(d.fields());
-	}
+    public boolean equals(Object o) {
+        if (!(o instanceof RecordDescr))
+            return false;
+        RecordDescr d = (RecordDescr) o;
+        return this.recSymbolTable.equals(d.fields());
+    }
 
-	public int hashCode() {
-		int result = 7;
-		result = 31 * result + recSymbolTable.hashCode();
-		return result;
-	}
+    public int hashCode() {
+        int result = 7;
+        result = 31 * result + recSymbolTable.hashCode();
+        return result;
+    }
 }
