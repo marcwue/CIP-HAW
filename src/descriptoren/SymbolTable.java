cf058861871e3ac85786d65a0ae52c1396e7cdf7
Benjamin Kahlau <ngk-ben@gmx.de>
2012-06-21 16:38:51 +0200

    SymbolTable reformatiert

--- src/descriptoren/SymbolTable.java
+++ src/descriptoren/SymbolTable.java
@@ -1,150 +1,152 @@
 /**
- * 
+ *
  */
 package descriptoren;
+
+import app.TokenID;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import app.TokenID;
-
 /**
  * @author Marc Wüseke
- * 
  */
 public class SymbolTable {
 
-	private Map<String, AbstractDescr> descriptorMap = new HashMap<String, AbstractDescr>();
-	private Map<String, Integer> addressMap = new HashMap<String, Integer>();
-	private int currentAddress = 0;
-	private int currentParameterAddress = -3; // muss spaeter geaendert werden
-												// -> wenn SL-Register eingebaut
-	private SymbolTable parentTable;
+    private Map<String, AbstractDescr> descriptorMap;
+    private Map<String, Integer> addressMap;
+    private int currentAddress = 0;
 
-	public SymbolTable() {
-		this.parentTable = null;
-	}
+    //Todo Sinn klären
+    private int currentParameterAddress = -3; // muss spaeter geaendert werden
+    // -> wenn SL-Register eingebaut
+    private SymbolTable parentTable;
 
-	/**
-	 * @param parentTable
-	 */
-	public SymbolTable(SymbolTable parentTable) {
-		this.parentTable = parentTable;
-	}
+    public SymbolTable() {
+        this.parentTable = null;
+        descriptorMap = new HashMap<>();
+        addressMap = new HashMap<>();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		String retS = "Descriptor Map:\n";
-		for (Entry<String, AbstractDescr> aD : descriptorMap.entrySet()) {
-			retS += aD.toString() + "\n";
-		}
-		retS += "Address Map:\n";
-		for (Entry<String, Integer> aM : addressMap.entrySet()) {
-			retS += aM.toString() + "\n";
-		}
-		return "SymbolTable\n" + retS + "Current Address:" + currentAddress;
-	}
+    public SymbolTable(SymbolTable parentTable) {
+        this.parentTable = parentTable;
+        descriptorMap = new HashMap<>();
+        addressMap = new HashMap<>();
+    }
 
-	public void declare(String ident, AbstractDescr descr) {
-		if (!(addressMap.containsKey(ident))) {
-			descriptorMap.put(ident, descr);
-			addressMap.put(ident, currentAddress);
-			if (descr == null) {
-				System.out.println("---- compile Error-----\n Variable = "
-						+ ident);
-			}
-			currentAddress += descr.getSize();
-		} else {
-			System.out
-					.println("Fehler, zweimal die gleiche Variable deklariert");
-		}
-	}
+    /*
+      * (non-Javadoc)
+      *
+      * @see java.lang.Object#toString()
+      */
+    @Override
+    public String toString() {
+        String retS = "Descriptor Map:\n";
+        for (Entry<String, AbstractDescr> aD : descriptorMap.entrySet()) {
+            retS += aD.toString() + "\n";
+        }
+        retS += "Address Map:\n";
+        for (Entry<String, Integer> aM : addressMap.entrySet()) {
+            retS += aM.toString() + "\n";
+        }
+        return "SymbolTable\n" + retS + "Current Address:" + currentAddress;
+    }
 
-	public void declareType(String ident, AbstractDescr descr) {
-		if (!(descriptorMap.containsKey(ident))) {
-			descriptorMap.put(ident, descr);
-			addressMap.put(ident, -1);
-			if (descr == null) {
-				System.out.println("---- compile Error-----\n Variable = "
-						+ ident);
-			}
-		} else {
-			System.out
-					.println("Fehler, zweimal die gleiche Variable deklariert");
-		}
-	}
+    public void declare(String ident, AbstractDescr descr) {
+        if (!(addressMap.containsKey(ident))) {
+            descriptorMap.put(ident, descr);
+            addressMap.put(ident, currentAddress);
+            if (descr == null) {
+                System.out.println("---- compile Error-----\n Variable = " + ident);
+            }
+            // May return NullPointerException
+            currentAddress += descr.getSize();
+        } else {
+            System.out
+                    .println("Fehler, zweimal die gleiche Variable deklariert");
+        }
+    }
 
-	public void declareParams(String ident, AbstractDescr descr) {
-		if (!(addressMap.containsKey(ident))) {
-			currentParameterAddress = currentParameterAddress - descr.getSize();
-			addressMap.put(ident, currentParameterAddress);
-			descriptorMap.put(ident, descr);
-		} else {
-			System.out.println("Variable zweimal deklariert");
-		}
+    public void declareType(String ident, AbstractDescr descr) {
+        if (!(descriptorMap.containsKey(ident))) {
+            descriptorMap.put(ident, descr);
+            addressMap.put(ident, -1);
+            if (descr == null) {
+                System.out.println("---- compile Error-----\n Variable = "
+                        + ident);
+            }
+        } else {
+            System.out
+                    .println("Fehler, zweimal die gleiche Variable deklariert");
+        }
+    }
 
-	}
+    public void declareParams(String ident, AbstractDescr descr) {
+        if (!(addressMap.containsKey(ident))) {
+            currentParameterAddress = currentParameterAddress - descr.getSize();
+            addressMap.put(ident, currentParameterAddress);
+            descriptorMap.put(ident, descr);
+        } else {
+            System.out.println("Variable zweimal deklariert");
+        }
 
-	public AbstractDescr descriptorFor(String ident) {
-		// built-in types
-		if (("integer").equalsIgnoreCase(ident)) {
-			return new SimpleTypeDescr(TokenID.INT);
-		} else if (("string").equalsIgnoreCase(ident)) {
-			return new SimpleTypeDescr(TokenID.STR);
-		} else if (("boolean").equalsIgnoreCase(ident)) {
-			return new SimpleTypeDescr(TokenID.BOOLEAN);
-		}
-		AbstractDescr d = descriptorMap.get(ident);
-		if (d == null && parentTable != null) {
-			return parentTable.descriptorFor(ident);
-		} else if (d == null) {
-			System.out.println("Deskriptor fuer " + ident + " nicht gefunden.");
-		}
-		return d;
-	}
+    }
 
-	public int addressOf(String ident) {
+    public AbstractDescr descriptorFor(String ident) {
+        // built-in types
+        if (("integer").equalsIgnoreCase(ident)) {
+            return new SimpleTypeDescr(TokenID.INT);
+        } else if (("string").equalsIgnoreCase(ident)) {
+            return new SimpleTypeDescr(TokenID.STR);
+        } else if (("boolean").equalsIgnoreCase(ident)) {
+            return new SimpleTypeDescr(TokenID.BOOLEAN);
+        }
+        AbstractDescr d = descriptorMap.get(ident);
+        if (d == null && parentTable != null) {
+            return parentTable.descriptorFor(ident);
+        } else if (d == null) {
+            System.out.println("Deskriptor fuer " + ident + " nicht gefunden.");
+        }
+        return d;
+    }
 
-		if (addressMap.containsKey(ident)) {
-			return addressMap.get(ident);
-		}
-		if (parentTable != null) {
-			return parentTable.addressOf(ident);
-		}
-		return -1;
+    public int addressOf(String ident) {
 
-	}
+        if (addressMap.containsKey(ident)) {
+            return addressMap.get(ident);
+        }
+        if (parentTable != null) {
+            return parentTable.addressOf(ident);
+        }
+        return -1;
 
-	public int getSize() {
-		return currentAddress;
-	}
+    }
 
-	public void link(String ident, String toIdent) {
-		if (addressMap.containsKey(ident) && addressMap.containsKey(toIdent)
-				&& descriptorFor(ident) == descriptorFor(toIdent)) {
-			addressMap.put(ident, addressOf(toIdent));
-		} else {
-			if (!addressMap.containsKey(ident))
-				System.out.println(ident + " isn't declarated");
-			if (!addressMap.containsKey(toIdent))
-				System.out.println(ident + " isn't declarated");
-			if (descriptorFor(ident) != descriptorFor(toIdent))
-				System.out.println("Cann't link address. " + ident + " and "
-						+ toIdent + " have different types.");
-		}
-	}
+    public int getSize() {
+        return currentAddress;
+    }
 
-	public int getLevel() {
-		if (this.parentTable == null)
-			return 0;
-		else
-			return 1 + parentTable.getLevel();
-	}
+    public void link(String ident, String toIdent) {
+        if (addressMap.containsKey(ident) && addressMap.containsKey(toIdent)
+                && descriptorFor(ident) == descriptorFor(toIdent)) {
+            addressMap.put(ident, addressOf(toIdent));
+        } else {
+            if (!addressMap.containsKey(ident))
+                System.out.println(ident + " isn't declarated");
+            if (!addressMap.containsKey(toIdent))
+                System.out.println(ident + " isn't declarated");
+            if (descriptorFor(ident) != descriptorFor(toIdent))
+                System.out.println("Cann't link address. " + ident + " and "
+                        + toIdent + " have different types.");
+        }
+    }
+
+    public int getLevel() {
+        if (this.parentTable == null)
+            return 0;
+        else
+            return 1 + parentTable.getLevel();
+    }
 
 }
